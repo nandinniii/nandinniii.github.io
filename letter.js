5c413d09c76300104c84b80aacc81fc06f8d5e89
nandinniii <125787746+nandinniii@users.noreply.github.com>
2023-02-19 23:00:07 +0530

    Add files via upload

---
+++ letter.js
@@ -0,0 +1,16 @@
+var i = 0;
+var txt1 = "Heluuu Miss Nandiniii...!  > Apologiesss for the delayyy. Alsoo, Idiot, > I know you. Toh stop >  justifying your actions when> you share your number with anyone. >> Thank youuu for being there when things were rough for mee. You legit have no clue how much you've helped mee. lately you havee been chaging your DPs a lot. Stating the obvious, you look amazing in all of themmm, with your smile being the prettiest among all!! Are you impressing that bumble guyy? XD >> The best thing about our friendship is that I can always trust youu Miss Nandini !! And thank youu for bearing mee and my super power to make things awkward. So here is my promiseee for ya, if you ever need helppp, I'll be there. Even if I'll be helpless, wouldn't let ya be alone and be right there by your side. >>  Now your turn. Help me smooth talk with women so that I'll have soooo many girlfriends. Or better help me find them XD";
+var speed = 50;
+typeWriter();
+function typeWriter() {
+    if (i < txt1.length) {        
+        if(txt1.charAt(i)=='<')
+        document.getElementById("text1").innerHTML += '</br>'
+        else if(txt1.charAt(i)=='>')
+            document.getElementById("text1").innerHTML += '</br>'
+        else
+        document.getElementById("text1").innerHTML += txt1.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
